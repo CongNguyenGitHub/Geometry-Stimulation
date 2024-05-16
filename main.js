768e76c994e2f6e26e6ff1570b2f35bc07f94a1e
unknown <21521200@gm.uit.edu.vn>
2024-05-16 21:48:39 +0700

    add more object and load model from file

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-import * as THREE from 'three'; // Thay đổi dường dẫn import 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Thay đổi đường dẫn import
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { TeapotGeometry } from 'three/examples/jsm/geometries/TeapotGeometry';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 function init() {
     var scene = new THREE.Scene();
-
+    var loader = new GLTFLoader();
     var geometry, material, mesh;
     material = new THREE.MeshBasicMaterial({ color: "#ffffff" });
 
@@ -29,6 +31,44 @@ function init() {
             case "Torus":
                 geometry = new THREE.TorusGeometry(4, 2, 16, 100);
                 break;
+            case "Teapot":
+                geometry=new TeapotGeometry(4, 10);
+                break;
+            case "Tube":
+                geometry = new THREE.TubeGeometry(getTube(6), 20, 2, 8, false);
+                break;
+            case "Heart":
+                geometry = new THREE.ExtrudeGeometry(getHeart(), { amount: 2, bevelEnable: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 });
+                break;
+            case "Car":
+                loader.load( './assets/model/car/scene.gltf', function ( gltf ) {
+                    scene.remove(scene.getObjectByName("geometry"));
+                    gltf.scene.scale.set(5,5,5);
+                    gltf.scene.castShadow=true;
+                    gltf.scene.name="geometry";
+                    scene.add(gltf.scene);
+                
+                }, undefined, function ( error ) {
+                
+                    console.error( error );
+                
+                } );
+                return;
+            case "Scifi_girl":
+                loader.load( './assets/model/scifi_girl/scene.gltf', function ( gltf ) {
+                    scene.remove(scene.getObjectByName("geometry"));
+                    gltf.scene.scale.set(5,5,5);
+                    gltf.scene.castShadow=true;
+                    gltf.scene.name="geometry";
+                    scene.add(gltf.scene);
+                
+                }, undefined, function ( error ) {
+                
+                    console.error( error );
+                
+                } );
+                return;
+
         }
         mesh = new THREE.Mesh(geometry, material);
 
@@ -38,8 +78,8 @@ function init() {
         mesh.castShadow = true; 
 
         scene.add(mesh);
+       
     });
-
 
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.set(10, 7, 20);
@@ -65,5 +105,38 @@ function update(renderer, scene, camera, controls) {
         update(renderer, scene, camera, controls);
     });
 }
+function getHeart() {
+    const x = -10,
+        y = -10;
+    var heartShape = new THREE.Shape();
+    heartShape.moveTo(x + 5, y + 5);
+    heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
+    heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
+    heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
+    heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
+    heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
+    heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
 
+    return heartShape;
+}
+
+function getTube(size) {
+    class CustomSinCurve extends THREE.Curve {
+        constructor(scale = 1) {
+            super();
+
+            this.scale = scale;
+        }
+
+        getPoint(t, optionalTarget = new THREE.Vector3()) {
+            const tx = t * 3 - 1.5;
+            const ty = Math.sin(2 * Math.PI * t);
+            const tz = 0;
+
+            return optionalTarget.set(tx, ty, tz).multiplyScalar(this.scale);
+        }
+    }
+
+    return new CustomSinCurve(size);
+}
 init();
