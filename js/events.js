6308f0af63ca8f1b0469824e897e57bd407be4e3
lmnguyet <lynda044@gmail.com>
2024-05-19 00:00:37 +0700

    import, export, UI v.1

--- js/events.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("reset").addEventListener("click", function() {
-    var confirmReset = confirm("Are you sure you want to reset the page?");
-    if (confirmReset) {
-        location.reload();
-    }
-});
-
-
